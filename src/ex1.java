f2295697d6e6da90cb6985d89328e90e152cd04c
bluedog <bluedog129@gmail.com>
2021-08-30 22:39:53 +0900

    ex1 추가

---
+++ src/ex1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void fib(int count, int previous, int current, int n) {
+        if(count > n) {
+            return;
+        }
+        System.out.println(current);
+        fib(count + 1, current, previous + current, n);
+    }
+
+    public static void fib(int n) {
+        int count = 1;
+        fib(count, 0, 1, n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        fib(n);
+    }
+}
