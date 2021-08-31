1dd366c8e9f450ecd30966572b912b7bb17cd2cc
bluedog <bluedog129@gmail.com>
2021-08-31 20:54:26 +0900

    recur3 추가

---
+++ src/recur3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class recur3 {
+    //factorial(3)  6
+    //fac(4) 24
+    public static int fac(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return n * fac(n-1);
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+
+       System.out.println(fac(n));
+    }
+}
