bc47ddfb656252a52317c886d7cede0784d9ac83
bluedog <bluedog129@gmail.com>
2021-09-08 22:00:40 +0900

    src/queue/num1158.java

--- src/queue/num1158.java
+++ src/queue/num1158.java
@@ -18,9 +18,11 @@ public class num1158 {
 
         for (int i=0; i<n-1; i++) {
             for (int j = 0; j < m - 1; j++) {
+                System.out.println(queue);
                 queue.offer(queue.poll());
             }
             sb.append(queue.poll() + ", ");
+            System.out.println(sb);
         }
         sb.append(queue.poll() + ">");
         System.out.println(sb);
