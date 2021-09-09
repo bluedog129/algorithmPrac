6d53e798ef69819a845d34888b3c13d21fa20af0
bluedog <bluedog129@gmail.com>
2021-09-09 17:55:34 +0900

    num9023 추가

---
+++ src/stack/num9023.java
@@ -0,0 +1,32 @@
+package stack;
+
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class num9023 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bf.readLine());
+
+        while (t-- > 0) {
+            String str =bf.readLine() + "\n";
+            Stack<Character> s = new Stack<>();
+            for (char ch : str.toCharArray()) {
+                if(ch == '\n' || ch == ' ') {
+                    while (!s.isEmpty()) {
+                        bw.write(s.pop());
+                    }
+                    bw.write(ch);
+                } else {
+                    s.push(ch);
+                }
+            }
+        }
+        bw.flush();
+    }
+}
