ee2e3b070647bc95a418d00f860956cd4f9bbbdc
bluedog <bluedog129@gmail.com>
2021-09-09 21:50:11 +0900

    num2609 추가

--- src/modularArithmetic/num2609.java
+++ src/modularArithmetic/num2609.java
@@ -1,2 +1,23 @@
-package modularArithmetic;public class num2609 {
+package modularArithmetic;
+
+import java.util.Scanner;
+
+public class num2609 {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(a, a%b);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int g = gcd(a, b);
+        int l = a * b / g;
+        System.out.println(g);
+        System.out.println(l);
+    }
 }
