a068b4e142c235f2350e2738b23b5184f7b669a2
bluedog <bluedog129@gmail.com>
2021-09-10 19:07:11 +0900

    num10872 추가

---
+++ src/modularArithmetic/num10872.java
@@ -0,0 +1,19 @@
+package modularArithmetic;
+
+import java.util.Scanner;
+
+public class num10872 {
+    public static int fac(int n) {
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+
+        return n * fac(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fac(n));
+    }
+}
