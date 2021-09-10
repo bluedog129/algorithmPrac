eb5eb823bd5ff82f5d71e97ae6f4f766c3bb0e39
bluedog <bluedog129@gmail.com>
2021-09-10 20:56:21 +0900

    num1676 통과되는 코드로 수정

--- src/modularArithmetic/num1676.java
+++ src/modularArithmetic/num1676.java
@@ -1,29 +1,19 @@
 package modularArithmetic;
 
 import java.util.Scanner;
-
+// 직접 (0 ≤ N ≤ 500)의 N!를 구하면 시간초과가 일어난다.
+// 따라서 인수분해 했을 경우 2*5의 경우 0이 생긴다는 것을 고려하고
+// 2보다 5의 수가 더 적다는 것을 고려해 5의 배수의 갯수를 탐색.
+// 단, 5의 제곱수는 2개로 계산
 public class num1676 {
-    public static int fac(int n) {
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-
-        return n * fac(n-1);
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int cnt = 0;
-        int m = 1;
-
-        while(true) {
-            if(fac(n)%Math.pow(10,m) == 0) {
-                cnt++;
-            } else {
-                break;
-            }
-            m++;
+        int ans = 0;
+        for (int i=5; i<=n; i*=5) {
+            ans += n/i;
         }
-        System.out.println(cnt);
+        System.out.println(ans);
     }
 }
