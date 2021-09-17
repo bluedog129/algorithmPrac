43d50a50de01139168e2fbf60d36da2e934c75f4
bluedog <bluedog129@gmail.com>
2021-09-17 15:37:23 +0900

    num17103 추가

--- src/modularArithmetic/num17103.java
+++ src/modularArithmetic/num17103.java
@@ -1,2 +1,35 @@
-package modularArithmetic;public class num17103 {
+package modularArithmetic;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class num17103 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> primes = new ArrayList<>();
+        boolean[] check = new boolean[1000001];
+        for (int i = 2; i<=1000000; i++) {
+            if(check[i] == false) {
+                primes.add(i);
+                for (int j=i+i; j<=1000000; i+=i) {
+                    check[j] = true;
+                }
+            }
+        }
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int ans = 0;
+            for (int p : primes) {
+                if (n-p >= 2 && p <= n-p) {
+                    if (check[n-p] == false) {
+                        ans += 1;
+                    }
+                } else {
+                    break;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
 }
