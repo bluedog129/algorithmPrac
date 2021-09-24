a5e897234db0dadd4d9a1b402a1656348d85f640
bluedog <bluedog129@gmail.com>
2021-09-24 16:11:16 +0900

    해쉬맵 연습

---
+++ src/hash/hashmap.java
@@ -0,0 +1,37 @@
+package hash;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("myId", "1234");
+        map.put("asdf", "1111");
+        map.put("asdf", "1234");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("id와 password를 입력해주세요");
+            System.out.print("id :");
+            String id = sc.nextLine().trim(); // trim()은 공백제거
+
+            System.out.print("password :");
+            String password = sc.nextLine().trim();
+            System.out.println();
+
+            if(!map.containsKey(id)) {
+                System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요");
+                continue;
+            }
+
+            if(!(map.get(id).equals(password))) {
+                System.out.println("비밀번호가 일치하지 않습니다. 다시입력해주세요.");
+            } else {
+                System.out.println("id와 비밀번호가 일치합니다.");
+                break;
+            }
+        }
+    }
+}
