7db4420b06609cfb2e116d7fb93b31bdcde726ba
bluedog <bluedog129@gmail.com>
2021-09-29 00:36:13 +0900

    K번째 수 추가

---
+++ src/sort/kthNum.java
@@ -0,0 +1,32 @@
+package sort;
+import java.util.*;
+
+public class kthNum {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        System.out.println(Arrays.toString(solution(array, commands)));
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int index = 0;
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+
+            int[] tmp = new int[end - start + 1];
+
+            int a = 0;
+            for (int j = start - 1; j < end; j++)
+                tmp[a++] = array[j];
+
+            Arrays.sort(tmp);
+            answer[index++] = tmp[k - 1];
+        }
+        return answer;
+    }
+}
