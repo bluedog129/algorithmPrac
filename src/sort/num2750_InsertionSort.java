e3a16b7c2a6296420a44e83cb4a5a2871d0fe3af
bluedog <bluedog129@gmail.com>
2021-09-30 02:06:15 +0900

    삽입정렬문제

---
+++ src/sort/num2750_InsertionSort.java
@@ -0,0 +1,29 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class num2750_InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int j = 0;
+        int key = 0;
+        for (int i = 1; i < n; i++) {
+            key = arr[i];
+            j = i-1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
